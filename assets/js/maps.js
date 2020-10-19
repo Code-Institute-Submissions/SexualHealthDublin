94aecfb7869c64981bcf08eed1dfaf773dc9a231
Maggie McAlister <mcalistm@tcd.ie>
2020-10-19 20:43:34 +0000

    Added more markers to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -13,6 +13,10 @@ function initMap() {
             { lat: 53.342150, lng: -6.279260 }, //https://sticlinicdublin.ie/contact/
             { lat: 53.341020, lng: -6.259620 }, //https://www.trinityclinic.ie/contact-us-1
             { lat: 53.346920, lng: -6.265770 }, //https://jervismedical.ie/sti-testing-dublin-sexual-health/
+            { lat: 53.339955, lng: -6.296728}, //St. James's Hospital 
+            { lat: 53.358673, lng: -6.267815}, //https://www.materprivate.ie/dublin/centre-services/all-services/sexual-health-clinic/
+            { lat: 53.347634, lng: -6.268246}, //https://wellwomancentre.ie/health-matters/sexual-health/
+            { lat: 53.349711, lng: -6.278260}, //https://chartermedical.ie/contact-us/location-parking/
         ];
 
         var markers = locations.map(function(location, i) {
