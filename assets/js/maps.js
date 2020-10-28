dd7403a0c504151cc84353e5e7183b6849c8ede8
Maggie McAlister <mcalistm@tcd.ie>
2020-10-28 11:43:21 +0000

    Added displayed info for map markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,34 +1,51 @@
 function initMap() {
-        var map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 10,
-            center: {
-                lat: 53.343792,
-                lng: -6.254572
-            }
-        });
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: {
+        lat: 53.343792,
+        lng: -6.254572
+        }
+    });
+  var infoWin = new google.maps.InfoWindow();
+  var markers = locations.map(function(location, i) {
+    var marker = new google.maps.Marker({
+      position: location
+    });
+    google.maps.event.addListener(marker, 'click', function(evt) {
+      infoWin.setContent(location.info);
+      infoWin.open(map, marker);
+    })
+    return marker;
+  });
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
+}
+var locations = [
+    { lat: 53.342150, 
+      lng: -6.279260,
+      info: "Himerus Health<br>4th Floor, The Meath Primary Care Centre<br>Camden Clinic, Heytesbury Street, D8<br>Mon-Fri: 8:30-21:00<br>Sat: 10:00-13:00" }, //https://sticlinicdublin.ie/contact/
+    { lat: 53.341020,
+      lng: -6.259620, 
+      info: "Trinity Clinic<br>1st Floor, 27 South Anne Street, D2<br>Mon-Fri: 8:30-17:00<br>Walk-ins Welcome" }, //https://www.trinityclinic.ie/contact-us-1
+    { lat: 53.346920, 
+      lng: -6.265770, 
+      info: "Jervis Medical Centre<br>1 Jervis Street, D1<br>Mon-Fri: 8.30–18:00" }, //https://jervismedical.ie/sti-testing-dublin-sexual-health/
+    { lat: 53.339955, 
+      lng: -6.296728, 
+      info: "The GUIDE Clinic - Hospital 5<br>St. James's Hospital<br>James's Street, D8<br>Currently closed for STI screening walk-ins, clinic times vary"}, //St. James's Hospital 
+    { lat: 53.358673, 
+      lng: -6.267815, 
+      info: "Mater Private Hospital Sexual Health Clinic,<br> 44 Eccles Street, D7<br>Referral required for appointment"}, //https://www.materprivate.ie/dublin/centre-services/all-services/sexual-health-clinic/
+    { lat: 53.347083, 
+      lng: -6.263484, 
+      info: "Well Woman Centre - Liffey Street<br>35 Liffey Street Lower, D1<br>Hours vary, book online"}, //https://wellwomancentre.ie/health-matters/sexual-health/
+    { lat: 53.331955, 
+      lng: -6.237036, 
+      info: "Well Woman Centre - Pembroke Road<br>67 Pembroke Rd, D7<br>Hours vary, book online"},
+    { lat: 53.395260, 
+      lng: -6.214270, 
+      info: "Well Woman Centre - Coolock<br>Northside Shopping Centre, Oscar Traynor Rd, Coolock, D17<br>Hours vary, book online"}
+];
 
-        var labels = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
-
-        var locations = [
-            { lat: 53.342150, lng: -6.279260 }, //https://sticlinicdublin.ie/contact/
-            { lat: 53.341020, lng: -6.259620 }, //https://www.trinityclinic.ie/contact-us-1
-            { lat: 53.346920, lng: -6.265770 }, //https://jervismedical.ie/sti-testing-dublin-sexual-health/
-            { lat: 53.339955, lng: -6.296728}, //St. James's Hospital 
-            { lat: 53.358673, lng: -6.267815}, //https://www.materprivate.ie/dublin/centre-services/all-services/sexual-health-clinic/
-            { lat: 53.347634, lng: -6.268246}, //https://wellwomancentre.ie/health-matters/sexual-health/
-            { lat: 53.349711, lng: -6.278260}, //https://chartermedical.ie/contact-us/location-parking/
-        ];
-
-        var markers = locations.map(function(location, i) {
-            return new google.maps.Marker({
-                position: location,
-                label: labels[i % labels.length]
-            });
-        });
-
-        new MarkerClusterer(map, markers, {
-          imagePath:
-            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-        });
-
-    }
+google.maps.event.addDomListener(window, "load", initMap);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,3 @@
-
+$(document).ready(function(){
+    $("services").slideToggle('slow');
+});
