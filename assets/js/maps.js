bfc109a0a6c72b414107429085ef190f8cf8765f
Maggie McAlister <mcalistm@tcd.ie>
2020-10-28 12:00:06 +0000

    Added new location markers to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -45,7 +45,13 @@ var locations = [
       info: "Well Woman Centre - Pembroke Road<br>67 Pembroke Rd, D7<br>Hours vary, book online"},
     { lat: 53.395260, 
       lng: -6.214270, 
-      info: "Well Woman Centre - Coolock<br>Northside Shopping Centre, Oscar Traynor Rd, Coolock, D17<br>Hours vary, book online"}
+      info: "Well Woman Centre - Coolock<br>Northside Shopping Centre, Oscar Traynor Rd, Coolock, D17<br>Hours vary, book online"},
+    { lat: 53.357816, 
+      lng: -6.265193,
+      info: "MPower Community Rapid HIV Testing<br>70 Eccles Street, D7<br>Note: Services for gbMSM only"},
+    { lat: 53.349919,
+      lng: -6.269466,
+      info: "Gay Men's Health Service - Outhouse<br>105 Capel Street, D1<br>3rd Sunday of the month<br>13:30-15:00 or when slots fill up<br>Note: This is a drop-in service for gay and bisexual men,<br>MSM and transgender women"},
 ];
 
 google.maps.event.addDomListener(window, "load", initMap);
