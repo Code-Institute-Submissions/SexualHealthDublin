9e27fb03bf09d1439f4081a0c73e2da8b9fbe709
Maggie McAlister <mcalistm@tcd.ie>
2020-10-31 16:19:15 +0000

    Added and applied .img

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
-$(".open").on("click", function() {
-  $(".popup-overlay, .popup-content").addClass("active");
-});
- 
-$(".close, .popup-overlay").on("click", function() {
-  $(".popup-overlay, .popup-content").removeClass("active");
+$(document).ready(function(){
+
+    $(".open").on("click", function() {
+        $(".popup-overlay, .popup-content").addClass("active");
+    });
+    
+    $(".close, .popup-overlay").on("click", function() {
+        $(".popup-overlay, .popup-content").removeClass("active");
+    });
+
+    $(".open").on("click", function(){
+        $(".popup, .popup-content").addClass("active");
+    });
+
+    $(".close, .popup").on("click", function(){
+        $(".popup, .popup-content").removeClass("active");
+    });
+
 });
